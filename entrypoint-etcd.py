41be10319dd89abebc94e62dffa31f0caedaa031
szpone <nikola.adamus@gmail.com>
2019-03-06 22:06:07 +0100

    FIX: fix ports

--- entrypoint-etcd.py
+++ entrypoint-etcd.py
@@ -2,10 +2,13 @@
 import etcd
 
 is_ready = False
-client = etcd.Client(host='etcd', port=2879)
+client = etcd.Client(host='etcd', port=2379)
 
 while not is_ready:
+    print(is_ready)
+    print("dupa")
     try:
+        print('dupa')
         os.environ['POSTGRES_USER'] = client.read('/db_data/user').value
         os.environ['POSTGRES_DB'] = client.read('/db_data/user').value
         os.environ['POSTGRES_PASSWORD'] = client.read('/db_data/pswd').value
@@ -13,6 +16,6 @@ while not is_ready:
     except:
     	is_ready = False
 
-
+print(is_ready)
 if is_ready:
     os.execvp('docker-entrypoint.sh', ('docker-entrypoint.sh', *sys.argv[1:]))
