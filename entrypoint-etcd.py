9fd929c482c1bec76cad4a0558190b4406c2a0c1
szpone <nikola.adamus@gmail.com>
2019-03-07 09:02:31 +0100

    UPD. Update script

--- entrypoint-etcd.py
+++ entrypoint-etcd.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import etcd
+import os
 
 is_ready = False
 client = etcd.Client(host='etcd', port=2379)
 
 while not is_ready:
-    print(is_ready)
-    print("dupa")
     try:
-        print('dupa')
         os.environ['POSTGRES_USER'] = client.read('/db_data/user').value
-        os.environ['POSTGRES_DB'] = client.read('/db_data/user').value
+        os.environ['POSTGRES_DB'] = client.read('/db_data/name').value
         os.environ['POSTGRES_PASSWORD'] = client.read('/db_data/pswd').value
         is_ready = True
     except:
