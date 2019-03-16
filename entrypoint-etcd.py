90c97d6577d0f70b7cf241c49638e0753b30946e
szpone <nikola.adamus@gmail.com>
2019-03-16 16:50:23 +0100

    UPD: refactor entrypont

--- entrypoint-etcd.py
+++ entrypoint-etcd.py
@@ -6,12 +6,10 @@ from secrets import choice
 from string import ascii_letters, digits
 
 pswd  = ascii_letters + digits
-
 secret_key = ''.join(choice(pswd) for i in range(20))
 db_password = ''.join(choice(pswd) for i in range(20))
 
 setup_completed = False
-is_ready = False
 client = etcd.Client(host='etcd', port=2379, allow_reconnect=True)
 
 while not setup_completed:
@@ -20,19 +18,14 @@ while not setup_completed:
         client.read('/setup_completed')
         setup_completed = True
     except etcd.EtcdKeyNotFound:
-        setup_completed = False
         client.write('/django', secret_key)
         client.write('/db_data/user', 'admin')
         client.write('/db_data/name', 'baza')
         client.write('/db_data/pswd', db_password)
         client.write('/setup_completed', 1)
 
-if setup_completed:
+os.environ['POSTGRES_USER'] = client.read('/db_data/user').value
+os.environ['POSTGRES_DB'] = client.read('/db_data/name').value
+os.environ['POSTGRES_PASSWORD'] = client.read('/db_data/pswd').value
 
-    os.environ['POSTGRES_USER'] = client.read('/db_data/user').value
-    os.environ['POSTGRES_DB'] = client.read('/db_data/name').value
-    os.environ['POSTGRES_PASSWORD'] = client.read('/db_data/pswd').value
-    is_ready = True
-
-if is_ready:
-    os.execvp('docker-entrypoint.sh', ('docker-entrypoint.sh', *sys.argv[1:]))
+os.execvp('docker-entrypoint.sh', ('docker-entrypoint.sh', *sys.argv[1:]))
